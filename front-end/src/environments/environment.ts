ec22fe52d8f66089318d1e354daa9cf65a3a4f42
LucasMartinelli <lucasbmartinelli@gmail.com>
2022-08-03 17:50:36 -0300

    fix(back): add system.properties

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'https://desafio-serasa.herokuapp.com/api/v1', //'http://localhost:8080/api/v1',
 };
 
 /*
